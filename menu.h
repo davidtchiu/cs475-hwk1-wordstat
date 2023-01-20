887e07087e02eeefce7e16ddf0c9bb5c8da56fc8
David Chiu <david.t.chiu@gmail.com>
2023-01-20 15:42:44 -0800

    menu constants

--- menu.h
+++ menu.h
@@ -8,6 +8,12 @@
 #ifndef MENU_H_
 #define MENU_H_
 
+// constants
+#define MENU_STATS 1 /** print word count */
+#define MENU_HISTO 2 /** print histogram */
+#define MENU_INPUT 3 /** return to inputting strings */
+#define MENU_EXIT 4  /** quit */
+
 // function declarations for the menu below
 int getMenuOption();
 
